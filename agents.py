9d9ff0df2d4614b619d763433703b8fef02c9fd1
SnehaDeshmukh28 <deshmusn@gmail.com>
2024-06-23 22:54:23 +0530

    model update

--- agents.py
+++ agents.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # Initialize the Gemini language model
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    #model="gemini-1.5-flash",
     verbose=True,
     temperature=0.5,
     google_api_key=os.getenv("GOOGLE_API_KEY")
